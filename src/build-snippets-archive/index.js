af753eae9b338eb3f6a8535d533931f2bbcb132f
Stefan Zweifel <hello@stefanzweifel.io>
2018-08-31 22:09:50 +0200

    Create a Patch Script

--- src/build-snippets-archive/index.js
+++ src/build-snippets-archive/index.js
@@ -1,4 +1,12 @@
-const snippets = require('./snippets.js');
+const createNewSnippets = require('./snippets.js');
 const archiveBuilder = require('./archiveBuilder.js');
+const patchNewSnippets = require('./patchNewSnippets.js');
 
-archiveBuilder(snippets());
+let snippets = createNewSnippets();
+
+// Create Archive without Patch
+archiveBuilder(snippets);
+
+// Create Archive with Patch
+// snippets = patchNewSnippets(snippets);
+// snippets.then(snippets => archiveBuilder(snippets));
---
+++ src/build-snippets-archive/patchNewSnippets.js
@@ -0,0 +1,28 @@
+const uuidv4 = require('uuid/v4');
+
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+
+module.exports = function (newSnippets) {
+
+    async function getOriginalVsNew() {
+        return await readFile('./original-emoji-pack/original-vs-new-snippets.json', 'utf-8');
+    }
+
+    return getOriginalVsNew().then(oldVsNew => {
+
+        JSON.parse(oldVsNew).forEach(snippet => {
+            newSnippets.push({
+                alfredsnippet: {
+                    snippet: snippet.originalSnippet.snippet,
+                    uid: uuidv4(),
+                    name: snippet.originalSnippet.name.replace(/:/g, ''),
+                    keyword: snippet.originalSnippet.keyword,
+                }
+            });
+        });
+
+        return newSnippets;
+    });
+}
